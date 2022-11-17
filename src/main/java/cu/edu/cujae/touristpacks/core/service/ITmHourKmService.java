fc0e1072dfc2af218c28d2257274e22b58a70ca0
Grenier8 <grjuako18@gmail.com>
2022-11-17 18:48:15 -0500

    Davian's commit. His Services and Controllers

--- src/main/java/cu/edu/cujae/touristpacks/core/service/ITmHourKmService.java
+++ src/main/java/cu/edu/cujae/touristpacks/core/service/ITmHourKmService.java
@@ -1,19 +1,20 @@
 package cu.edu.cujae.touristpacks.core.service;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import cu.edu.cujae.touristpacks.core.dto.TmHourKmDto;
 
 public interface ITmHourKmService {
-    List<TmHourKmDto> getTmHourKms();
+    List<TmHourKmDto> getTmHourKms() throws SQLException;
 
-    TmHourKmDto getTmHourKmById(int tmHourKmId);
+    TmHourKmDto getTmHourKmById(int tmHourKmId) throws SQLException;
 
-    TmHourKmDto getTmHourKmByName(String tmHourKmName);
+    TmHourKmDto getTmHourKmByName(String tmHourKmName) throws SQLException;
 
-    void createTmHourKm(TmHourKmDto tmHourKm);
+    void createTmHourKm(TmHourKmDto tmHourKm) throws SQLException;
 
-    void updateTmHourKm(TmHourKmDto tmHourKm);
+    void updateTmHourKm(TmHourKmDto tmHourKm) throws SQLException;
 
-    void deleteTmHourKm(int idTmHourKm);
+    void deleteTmHourKm(int idTmHourKm) throws SQLException;
 }
--- src/main/java/cu/edu/cujae/touristpacks/core/service/ITransportContractService.java
+++ src/main/java/cu/edu/cujae/touristpacks/core/service/ITransportContractService.java
@@ -1,19 +1,20 @@
 package cu.edu.cujae.touristpacks.core.service;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import cu.edu.cujae.touristpacks.core.dto.TransportContractDto;
 
 public interface ITransportContractService {
-    List<TransportContractDto> getTransportContracts();
+    List<TransportContractDto> getTransportContracts() throws SQLException;
 
-    TransportContractDto getTransportContractById(int transportContractId);
+    TransportContractDto getTransportContractById(int transportContractId) throws SQLException;
 
-    TransportContractDto getTransportContractByTitle(String transportContracTitle);
+    TransportContractDto getTransportContractByTitle(String transportContracTitle) throws SQLException;
 
-    void createTransportContract(TransportContractDto transportContract);
+    void createTransportContract(TransportContractDto transportContract) throws SQLException;
 
-    void updateTransportContract(TransportContractDto transportContract);
+    void updateTransportContract(TransportContractDto transportContract) throws SQLException;
 
-    void deleteTransportContract(int idTransportContract);
+    void deleteTransportContract(int idTransportContract) throws SQLException;
 }
